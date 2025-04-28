1ee6a269214cbb9352bff7ff3538b34acf6d3db4
Eric machado <eric.13formiga@gmail.com>
2025-04-28 11:54:11 -0300

    feat: query params

--- src/server.ts
+++ src/server.ts
@@ -7,8 +7,17 @@ const app = express()
 app.get("/products/:id/:user", (request, reponse)=>{
 const { id,user } = request.params 
 reponse.send(`o Id é ${id} e o Usuário é ${user}`)
+
+
 })
 
+app.get("/products", (request, reponse)=>{
+   ///products?page=1&limit=10
+   const { page, limit } = request.query
+   reponse.send(`a pagina é ${page} e o imite é ${limit}`)
+   
+   
+   })
 app.listen(PORT,()=>{
    console.log(`Server is running at ${PORT}`);
 })
