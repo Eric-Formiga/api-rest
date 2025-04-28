3c791d784aba8591b7a9cb57cb2c25ae0f15e262
Eric Machado <eric.machado@imagecenter.inf.br>
2025-04-28 10:17:41 -0300

    feat: First route GET

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ const PORT = 3333
 
 const app = express()
 
+app.get("/", (request, reponse)=>{
+reponse.send("Hello World")
+})
+
 app.listen(PORT,()=>{
    console.log(`Server is running at ${PORT}`);
 })
