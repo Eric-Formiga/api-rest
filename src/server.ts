8bbc94e6620db7ff28441b46e96089effee78a54
Eric machado <eric.13formiga@gmail.com>
2025-04-28 12:33:10 -0300

    feat: Post Route

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,11 @@ const PORT = 3333
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/products/:id/:user", (request, reponse)=>{
 const { id,user } = request.params 
 reponse.send(`o Id é ${id} e o Usuário é ${user}`)
-
 
 })
 
@@ -16,6 +17,11 @@ app.get("/products", (request, reponse)=>{
    const { page, limit } = request.query
    reponse.send(`a pagina é ${page} e o imite é ${limit}`)
    
+})
+
+app.post("/products", (request, reponse)=>{
+   const { name,lastName } = request.body
+   reponse.send(`Nome completo:${name} ${lastName} `)
    
    })
 app.listen(PORT,()=>{
