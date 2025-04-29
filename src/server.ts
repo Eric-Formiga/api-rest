d674f9724ca3073c2f59e819fd0769902f192fe0
Eric-Formiga <eric.13formiga@gmail.com>
2025-04-29 18:58:20 -0300

    feat: add Zod Error

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import { routes } from "./routes";
 import { AppError } from "./utils/AppError";
+import { ZodError } from "zod";
 
 const PORT = 3333;
 
@@ -15,7 +16,12 @@ app.use((error: any, request: Request, response: Response, _: NextFunction) => {
         return response.status(error.statusCode).json({ message: error.message });
     }
 
+    if (error instanceof ZodError) {
+        return response.status(400).json({ message: "Validation error", issues: error.format() });
+    }
+
     response.status(500).json({ message: error.message });
+
 });
 
 app.listen(PORT, () => {
